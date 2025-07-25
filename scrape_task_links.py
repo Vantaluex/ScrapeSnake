459029f0ef62714c5c60ce1a99c1b3f20bce8f7f
vantaluex <vantaluex@gmail.com>
2025-07-25 16:08:16 +0900

    scrape_task_links added. this takes in the contest numbers and spits back the taskurls as a list.

--- scrape_task_links.py
+++ scrape_task_links.py
@@ -5,8 +5,7 @@ def scrape_task_links(contestnumber):
     contesturl = requests.get("http://atcoder.jp/contests/abc" + f"{contestnumber:03d}" + "/tasks")
     soup = BeautifulSoup(contesturl.text, "html.parser")
     taskurls = ["https://atcoder.jp" + td.find("a").get("href") for td in soup.findAll("td", attrs={"class":"text-center no-break"}) if td.find("a")]
-    for taskurl in taskurls:
-        print(taskurl)
+    # for taskurl in taskurls:
+    #     print(taskurl)
     return taskurls
     
-scrape_task_links(42)
